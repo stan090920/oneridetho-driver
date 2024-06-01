becbca928d7457dde8829050d0c5653fc780ef0f
Sydney <64653592+sydney06@users.noreply.github.com>
2024-06-01 22:58:21 +0300

    Reverted ride acceptance

--- pages/api/rides/accept/[rideId].ts
+++ pages/api/rides/accept/[rideId].ts
@@ -20,39 +20,20 @@ function formatTime(date: any) {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-    return;
-  }
+  if (req.method === 'POST') {
+    const rideIdStr = typeof req.query.rideId === 'string' ? req.query.rideId : null;
+    if (!rideIdStr) {
+      return res.status(400).json({ message: 'Invalid ride ID' });
+    }
+    const rideId = parseInt(rideIdStr);
 
-  const rideIdStr = typeof req.query.rideId === 'string' ? req.query.rideId : null;
-  if (!rideIdStr) {
-    return res.status(400).json({ message: 'Invalid ride ID' });
-  }
-  const rideId = parseInt(rideIdStr);
+    const driverId = typeof req.body.driverId === 'number' ? req.body.driverId : null;
+    if (driverId === null) {
+      return res.status(400).json({ message: 'Invalid driver ID' });
+    }
 
-  const driverId = typeof req.body.driverId === 'number' ? req.body.driverId : null;
-  if (driverId === null) {
-    return res.status(400).json({ message: 'Invalid driver ID' });
-  }
-
-  try {
-    await prisma.$transaction(async (prisma) => {
-      const ride = await prisma.ride.findUnique({
-        where: { id: rideId },
-        select: { isAccepted: true, status: true }
-      });
-
-      if (!ride) {
-        throw new Error('Ride not found');
-      }
-
-      if (ride.isAccepted || ['Completed', 'Cancelled', 'InProgress'].includes(ride.status)) {
-        throw new Error('Ride cannot be accepted');
-      }
-
-      await prisma.ride.update({
+    try {
+      const updatedRide = await prisma.ride.update({
         where: { id: rideId },
         data: {
           isAccepted: true,
@@ -63,35 +44,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           user: true
         }
       });
-    });
 
-    const updatedRide = await prisma.ride.findUnique({
-      where: { id: rideId },
-      include: {
-        driver: true,
-        user: true
-      }
-    });
+      if (updatedRide.driver && updatedRide.user && updatedRide.user.phone) {
+        let bodyMessage;
+        if (updatedRide.isScheduled && updatedRide.scheduledPickupTime) {
+          const formattedTime = formatTime(updatedRide.scheduledPickupTime);
+          bodyMessage = `Your scheduled ride for ${formattedTime} has been confirmed! Your driver ${updatedRide.driver.name} will arrive in a ${updatedRide.driver.carType} with license plate ${updatedRide.driver.licensePlate}. Please visit https://oneridetho-ten.vercel.app/rides/${updatedRide.id} for more details.\n\nHave a great trip!`;
+        } else {
+          bodyMessage = `Great news! Your ride with ${updatedRide.driver.name} has been confirmed. Your driver will be in a ${updatedRide.driver.carType} with license plate ${updatedRide.driver.licensePlate}. For more details about your ride, visit: https://oneridetho-ten.vercel.app/rides/${updatedRide.id}\n\nWe wish you a safe and pleasant journey!`;
+        }
 
-    if (updatedRide?.driver && updatedRide?.user && updatedRide.user.phone) {
-      let bodyMessage;
-      if (updatedRide.isScheduled && updatedRide.scheduledPickupTime) {
-        const formattedTime = formatTime(updatedRide.scheduledPickupTime);
-        bodyMessage = `Your scheduled ride for ${formattedTime} has been confirmed! Your driver ${updatedRide.driver.name} will arrive in a ${updatedRide.driver.carType} with license plate ${updatedRide.driver.licensePlate}. Please visit https://oneridetho-ten.vercel.app/rides/${updatedRide.id} for more details.\n\nHave a great trip!`;
-      } else {
-        bodyMessage = `Great news! Your ride with ${updatedRide.driver.name} has been confirmed. Your driver will be in a ${updatedRide.driver.carType} with license plate ${updatedRide.driver.licensePlate}. For more details about your ride, visit: https://oneridetho-ten.vercel.app/rides/${updatedRide.id}\n\nWe wish you a safe and pleasant journey!`;
+        await twilioClient.messages.create({
+          body: bodyMessage,
+          from: process.env.TWILIO_PHONE_NUMBER,
+          to: updatedRide.user.phone
+        });
       }
 
-      await twilioClient.messages.create({
-        body: bodyMessage,
-        from: process.env.TWILIO_PHONE_NUMBER,
-        to: updatedRide.user.phone
-      });
+      res.status(200).json({ message: 'Ride accepted successfully', updatedRide });
+    } catch (error) {
+      console.error('Error accepting the ride:', error);
+      res.status(500).json({ message: 'Error accepting the ride' });
     }
-
-    res.status(200).json({ message: 'Ride accepted successfully', updatedRide });
-  } catch (error: any) {
-    console.error('Error accepting the ride:', error);
-    res.status(500).json({ message: 'Error accepting the ride', error: error.message });
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
